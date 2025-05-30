09beea56782838c84938edf083260cca693c1252
ioan-dispendix <ioan.cirea@dispendix.com>
2025-05-30 18:27:26 +0200

    fix name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use nelu_minigrep::Config;
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
@@ -9,7 +9,7 @@ fn main() {
         process::exit(1);
     });
     
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = nelu_minigrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
